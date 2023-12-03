698d90266909cdac625f311d0b97fa409034cc75
Priyanka Ghansela <priyanka.ghanselaa@gmail.com>
2023-12-03 20:15:49 +0530

    Modified footer

--- src/pages/footer/index.jsx
+++ src/pages/footer/index.jsx
@@ -4,13 +4,13 @@ import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
 
 const index = () => {
   return (
-    <div className='grid grid-cols-4 bg-[#f1f2f4] w-full mt-[13rem] px-[10.5rem] py-[2rem] font-plus-jakarta-sans 
+    <div className='grid grid-cols-4 bg-[#f1f2f4] w-full mt-[13rem] px-[10rem] py-[2rem] font-plus-jakarta-sans 
       xxl:px-[2rem]
       xl:flex xl:flex-col xl:px-[40px] xl:gap-[24px] 
       sm:flex sm:flex-col sm:px-[40px] sm:gap-[24px]'>
       <div className=''>
         <div className='flex gap-[8px] mb-[1rem]'>
-          <button className="bg-[#fff] px-2 rounded-3"><FontAwesomeIcon icon={faCaretUp} className="text-[24px] font-bold"/></button>
+          <button className="bg-[#fff] px-1 py-0 rounded-3"><FontAwesomeIcon icon={faCaretUp} className="text-[24px] font-bold"/></button>
           <h4 className='text-[24px] font-bold text-[#2d2d2d]'>Milton</h4>
         </div>
         <h5 className='text-[#6b6b78] text-[16px]'>A short text explaining <br></br>why my startup is so cool</h5>
@@ -46,15 +46,15 @@ const index = () => {
             <h5 className='text-[#6b6b78] text-[16px]'>Press</h5>
         </div>
       </div>
-      <div className='gap-[5px]
+      <div className='
         xl:flex xl:flex-col xl:gap-[10px]
         sm:flex sm:flex-col sm:gap-[10px]'>
         <h3 className='text-[20px] font-bold text-[#2d2d2d] mb-[1rem]'>From the blog</h3>
-        <div className='flex flex-col gap-[5px]'>
-          <h5 className='text-[#6b6b78] text-[16px] hover:text-black hover:underline hover:underline-offset-4'>Mastering Your Schedule: Top Time Management Techniques for Balancing Work and Life</h5><br/>
-          <h5 className='text-[#6b6b78] text-[16px] hover:text-black hover:underline hover:underline-offset-4'>Organizing Your Calendar for Enhanced Productivity and Focus</h5><br/>
-          <h5 className='text-[#6b6b78] text-[16px] hover:text-black hover:underline hover:underline-offset-4'>The Power of Prioritization: Effective Strategies for Managing Your Time and Tasks</h5><br/>
-          <h5 className='text-[#6b6b78] text-[16px] hover:text-black hover:underline hover:underline-offset-4'>Maximizing Your Minutes: Essential Tips for Streamlining Your Daily Routine and Calendar</h5><br/>
+        <div className='flex flex-col'>
+          <h5 className='text-[#6b6b78] text-[16px] hover:text-black hover:underline hover:underline-offset-4 leading-5'>Mastering Your Schedule: Top Time Management Techniques for Balancing Work and Life</h5><br/>
+          <h5 className='text-[#6b6b78] text-[16px] hover:text-black hover:underline hover:underline-offset-4 leading-5'>Organizing Your Calendar for Enhanced Productivity and Focus</h5><br/>
+          <h5 className='text-[#6b6b78] text-[16px] hover:text-black hover:underline hover:underline-offset-4 leading-5'>The Power of Prioritization: Effective Strategies for Managing Your Time and Tasks</h5><br/>
+          <h5 className='text-[#6b6b78] text-[16px] hover:text-black hover:underline hover:underline-offset-4 leading-5'>Maximizing Your Minutes: Essential Tips for Streamlining Your Daily Routine and Calendar</h5><br/>
         </div>
       </div>
     </div>
